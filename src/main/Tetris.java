d49eeea17ff6bb8bff0c7f70fa8e909212c184bc
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-01-19 12:43:05 +0100

    Part 1-3: init
    Created stubs:
    Field class
    GamePiece class
    
    Tetris class:
    -> main()
    -> field and gamePiece fields and its getters

---
+++ src/main/Tetris.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Tetris {
+    private Field field;
+    private GamePiece gamePiece;
+
+    public Field getField() {
+        return field;
+    }
+
+    public GamePiece getGamePiece() {
+        return gamePiece;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
