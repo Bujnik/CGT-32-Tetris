e20aa0dec6cfb78cdc7ad87ffd0f0a8e0af804ca
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-01-28 20:56:28 +0100

    Parts 13 - 14
    Methods provided by CodeGym:
    
    Field:
    -> getValue()
    -> setValue()
    -> print()
    ____
    
    Implemented removeFullLines():
    -> Helping methods: checkLine(), moveLines()

--- src/main/Field.java
+++ src/main/Field.java
@@ -1,14 +1,20 @@
 package main;
 
+/**
+ * The Field class describes the Tetris game field
+ */
 public class Field {
+    // Width and height
     private int width;
     private int height;
+
+    // Matrix representing the field: 1 means that part of the field is occupied, 0 means it is available
     private int[][] matrix;
 
-    public Field(int width, int height) {
+    public Field(int height, int width) {
         this.width = width;
         this.height = height;
-        this.matrix = new int[height][width];
+        matrix = new int[height][width];
     }
 
     public int getWidth() {
@@ -23,14 +29,111 @@ public class Field {
         return matrix;
     }
 
-    public void print(){}
+    /**
+     * The method returns the value of the matrix at coordinates (x, y)
+     * If the coordinates are outside the matrix, the method returns null.
+     */
+    public Integer getValue(int x, int y) {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
 
-    public void removeFullLines(){}
-
-    public Integer getValue(int x, int y){
-        return 0;
+        return null;
     }
 
-    public void setValue(int x, int y, int value){
+    /**
+     * The method sets the matrix cell with coordinates (x, y) to the passed value
+     */
+    public void setValue(int x, int y, int value) {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            matrix[y][x] = value;
+    }
+
+    /**
+     * The method displays the current contents of the matrix on the screen
+     */
+    public void print() {
+        // Create an array where we will "draw" the current game state
+        int[][] canvas = new int[height][width];
+
+        // Copy the game field matrix into the array
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                canvas[i][j] = matrix[i][j];
+            }
+        }
+
+        // Copy the game piece into the array, only the non-empty cells
+        int left = Tetris.game.getGamePiece().getX();
+        int top = Tetris.game.getGamePiece().getY();
+        int[][] brickMatrix = Tetris.game.getGamePiece().getMatrix();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (top + i >= height || left + j >= width) continue;
+                if (brickMatrix[i][j] == 1)
+                    canvas[top + i][left + j] = 2;
+            }
+        }
+
+
+        // Display what we've "drawn", but start from the edge of the frame.
+        System.out.println("---------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int index = canvas[i][j];
+                if (index == 0)
+                    System.out.print(" . ");
+                else if (index == 1)
+                    System.out.print(" X ");
+                else if (index == 2)
+                    System.out.print(" X ");
+                else
+                    System.out.print("???");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    /**
+     * Remove the completed lines
+     */
+    public void removeFullLines() {
+        for (int i = 0; i < matrix.length; i++) {
+            if (checkLine(matrix[i])) {
+                matrix[i] = null;
+                moveLines(matrix, i);
+            }
+        }
+    }
+
+    /**
+     * This method moves all lines down to replace deleted one
+     * New empty line is created on the top
+     */
+    private void moveLines(int[][] matrix, int index) {
+        for (int i = index; i >= 0; i--) {
+            if (matrix[i] != null) continue;
+            if (i > 0) {
+                //Swap current null row with row above
+                matrix[i] = matrix[i - 1];
+                matrix[i - 1] = null;
+            }
+            if (i == 0) matrix[i] = new int[width];
+        }
+    }
+
+    /**
+     * This method checks if all fields in a row are equal to 1
+     */
+    private boolean checkLine(int[] arr) {
+        for (int i : arr) {
+            if (i != 1) return false;
+        }
+        return true;
     }
 }
